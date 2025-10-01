b7e833aa939c3dfa51c1307a7431450e6eae5d0b
beauty-force <superk961106@gmail.com>
2025-10-01 18:31:49 -0500

    change delivery email, prompt

---
+++ public/build/assets/ChevronRightIcon.f66022a7.js
@@ -0,0 +1,1 @@
+import{o as e,c as r,d as o}from"./app.31cf4707.js";function s(t,n){return e(),r("svg",{xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24","stroke-width":"1.5",stroke:"currentColor","aria-hidden":"true"},[o("path",{"stroke-linecap":"round","stroke-linejoin":"round",d:"M8.25 4.5l7.5 7.5-7.5 7.5"})])}export{s as r};
